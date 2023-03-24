c3fa6880f05133210c1bad768d88eeaeffa45eb1
Faisal Richard Jr <faisalrichardjr@gmail.com>
2023-03-25 00:07:08 +0800

    added a simple temporary encryption algorithm

--- authentication.py
+++ authentication.py
@@ -2,8 +2,17 @@ def login():
     pass
 
 
-def encrypt():
-    pass
+def encrypt(plaintext):
+
+    # Basic encryption algorithm (will change/modify in the future)
+    ciphertext = ""
+    for character in plaintext.upper():
+        if character.isalpha() == True:
+            cipherascii = ord(character)
+            shift = ((1 + (cipherascii - ord('A')))) % 26 + ord('A')
+            ciphertext += chr(shift)
+
+    return ciphertext
 
 
 def decrypt():
