a25dc396f0dd5287f7abd2c8da970b30bee5b6c6
Faisal Richard Jr <faisalrichardjr@gmail.com>
2023-03-30 21:16:00 +0800

    .

---
+++ test.py
@@ -0,0 +1,10 @@
+import authentication as auth
+
+
+username = "abc"
+password = "cringe"
+
+username_encrypted = auth.encrypt(username)
+password_encrypted = auth.encrypt(username)
+
+print(username_encrypted)
