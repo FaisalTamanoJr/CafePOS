ff38db9dfaa6ceb3b2ca5728030a464989898ea0
BIENNE REESE YU <yubiennereese@gmail.com>
2023-04-03 11:08:35 +0800

    added all the items in the menu

--- menu.py
+++ menu.py
@@ -5,6 +5,31 @@ import orders
     note: more info in the diagrams at the docs
     """
 def display_menu():
+    #'Drink type': (Name, Price, Stocks)
+    'Expresso 1': (Mocha, 3.00, 20),
+    'Expresso 2': (Latte, 4.00, 20),
+    'Expresso 3': (Cappuccino, 3.50, 20),
+    'Expresso 4': (Americano, 4.50, 20),
+    'Expresso 5': (Espresso, 4.25, 20),
+    'Expresso 6': (Macchiato, 4.00, 20),
+    'Expresso 7': (Vanilla, 3.75, 20),
+
+    'Ice Blended 1': (Caramel, 4.25, 20),
+    'Ice Blended 2': (Coffee Latte, 3.50, 20),
+    'Ice Blended 3': (Frappe, 3.75, 20),
+    'Ice Blended 4': (Mocha, 4.00, 20),
+    'Ice Blended 5': (Mint, 5.00, 20),
+    'Ice Blended 6': (Matcha Cream, 4.50, 20),
+
+    'Tea 1': (Jasmine, 3.75, 20),
+    'Tea 2': (Green, 4.50, 20),
+    'Tea 3': (Earl Grey, 4.00, 20),
+    'Tea 4': (English, 3.75, 20),
+
+    'Non-coffee 1': (Chocolate Frappe, 4.75, 20),
+    'Non-coffee 2': (Milk Brown Sugar, 4.00, 20),
+    'Non-coffee 3': (Thai Milktea, 3.75, 20),
+
     pass
 
     """
